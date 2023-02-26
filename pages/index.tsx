b9760121db8c9dba70535e9f641ada11a63d7019
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2023-02-26 08:25:20 +0700

    fix: typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home() {
         </div>
         <div className={styles.grid}>
           <Link
-            href="/play"
+            href="/intro"
             className={styles.card}
           >
             <h2 className={inter.className}>
